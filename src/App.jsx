4e095c9c51479a67b1d03a859187bb49040aec40
Julia Finn <juliafinn02@gmail.com>
2026-03-30 18:12:37 -0400

    phone number

--- src/App.jsx
+++ src/App.jsx
@@ -318,7 +318,7 @@ function ContactPage() {
             <div className="contact-list">
               <div>
                 <h4>Phone</h4>
-                <p>(555) 000-0000</p>
+                <p>(917) 410-6526</p>
               </div>
               <div>
                 <h4>Email</h4>
@@ -488,7 +488,7 @@ function App() {
     <div className="footer-contact">
       <h4>Get In Touch</h4>
       <div className="footer-links">
-        <a href="tel:5550000000">(555) 000-0000</a>
+        <a href="tel:(917) 410-6526">(917) 410-6526</a>
         <NavLink className="button button-primary footer-button" to="/contact">
           Request a Bid
         </NavLink>
